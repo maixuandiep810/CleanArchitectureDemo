fe597b3eedbe32a955ba872d05672e1a09bcb18c
maixuandiep810 <maixuandiep810@gmail.com>
2021-08-29 21:56:21 +0700

    Init

---
+++ CleanArchitectureDemo.Infrastructure.Data/Context/LibraryDbContext.cs
@@ -0,0 +1,17 @@
+
+using CleanArchitecture.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Infrastructure.Data.Context
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        
+    }
+}
---
+++ CleanArchitectureDemo.UI.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CleanArchitectureDemo.UI.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
